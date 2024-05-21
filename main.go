b4b550189acb63d91f16e5b96c190a7efe7c3115
phor3nsic <phorensic@protonmail.com>
2024-05-21 14:00:07 +0100

    add ip/cidr

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"net"
 	"net/url"
 	"os"
 	"regexp"
@@ -18,10 +19,18 @@ func main() {
 	domainRegex := regexp.MustCompile(`^([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$`)
 	subdomainRegex := regexp.MustCompile(`^([a-zA-Z0-9-]+\.){2,}[a-zA-Z]{2,}$`)
 
+	// Regular expression to identify CIDR notation
+	cidrRegex := regexp.MustCompile(`^([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}$`)
+
 	// Function to check if the input is a URL
 	isURL := func(s string) bool {
 		parsedURL, err := url.ParseRequestURI(s)
 		return err == nil && (parsedURL.Scheme == "http" || parsedURL.Scheme == "https")
+	}
+
+	// Function to check if the input is an IP address
+	isIP := func(s string) bool {
+		return net.ParseIP(s) != nil
 	}
 
 	scanner := bufio.NewScanner(os.Stdin)
@@ -37,6 +46,10 @@ func main() {
 			fmt.Printf("[subdomain] %s\n", ninput)
 		case domainRegex.MatchString(ninput):
 			fmt.Printf("[domain] %s\n", ninput)
+		case isIP(ninput):
+			fmt.Printf("[ip] %s\n", ninput)
+		case cidrRegex.MatchString(ninput):
+			fmt.Printf("[cidr] %s\n", ninput)
 		default:
 			fmt.Println("Invalid input")
 		}
